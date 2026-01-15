81873c3b226ffea0c1822887d2b1af76d9ab3347
GabrielNatividadeBS <gabriel.natividade2011@gmail.com>
2026-01-15 20:20:42 -0300

    fix: backend CORS url origins

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,7 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://frontend-production-e235.up.railway.app"
 ]
 
 app.add_middleware(
